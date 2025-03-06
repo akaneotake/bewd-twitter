666a7289e99a4fb09456a08ae0181eb45740ed10
akaneotake <akane1033@gmail.com>
2025-03-06 18:06:59 +0700

    Migrate CreateTweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_06_102934) do
+ActiveRecord::Schema.define(version: 2025_03_06_110325) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "token", null: false
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2025_03_06_102934) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "message", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +39,5 @@ ActiveRecord::Schema.define(version: 2025_03_06_102934) do
   end
 
   add_foreign_key "sessions", "users"
+  add_foreign_key "tweets", "users"
 end
