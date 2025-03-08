8300751a585e23d8a4e64bf4f12c857e1a176280
akaneotake <akane1033@gmail.com>
2025-03-08 21:47:44 +0700

    Add POST /users API Endpoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :tweets, dependent: :destroy
 
+  has_secure_password  # bcrypt を利用してパスワードをハッシュ化
+
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 64 }
   validates :email, presence: true, uniqueness: true, length: { minimum: 5, maximum: 500 }
   validates :password, presence: true, length: { minimum: 8, maximum: 64 }
---
+++ app/views/users/create.jbuilder
@@ -0,0 +1,7 @@
+json.user do
+  json.id @user.id
+  json.username @user.username
+  json.email @user.email
+  json.created_at @user.created_at
+  json.updated_at @user.updated_at
+end
